31812f2ac52fdd8d37335229e8a75eb07e84132c
SPC <SPC@22-LAB402>
2024-07-01 19:38:12 +0700

    Add project files.

---
+++ CPP_SoNguyenTo/CPP_SoNguyenTo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "\nNhap n: ";
+    cin >> n;
+  
+    for (int i = 2; i <= n; i++)
+    {
+        bool snt = true;
+        for (int j = 2; j <= sqrt(i); j++)
+        {
+            if (i % j == 0) {
+                snt = false;
+                break;
+            }
+        }
+        if (snt) {
+            cout << i << " ";
+        }
+    }
+
+
+return 0;
+}
